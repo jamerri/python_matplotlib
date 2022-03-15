6d63e1874487b8fa1a1b3f48ab081a7174439b0d
jamerri <jamerri@163.com>
2022-03-16 01:00:30 +0800

    add 3.3

---
+++ 3_3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/3/16 12:59 上午
+# @Author : Jamerri
+# @FileName: 3_3.py
+# @Email : jamerri@163.com
+# @Software: PyCharm
+
+import matplotlib.pyplot as plt
